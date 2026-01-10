fef0ddc511f76271f6111754b04f4280d004fbb8
Romanos Binti <rmnsbinti966@gmail.com>
2026-01-10 00:44:09 -0500

    Updated files

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomePage = () => {
+    return (
+        <View>
+            <Text>HomePage</Text>
+        </View>
+    )
+}
+
+export default HomePage
+
+const styles = StyleSheet.create({})
